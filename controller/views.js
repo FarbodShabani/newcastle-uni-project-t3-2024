e7e1949ac641e75c8f110f0d58d87982e2dcb6a7
Farbod Shabani <shabani.farbod@yahoo.com>
2024-11-06 07:13:30 +1100

    routing for bug fix is done

--- controller/views.js
+++ controller/views.js
@@ -8,7 +8,27 @@ const homePageController = (req, res) => res.render(pagesList[0], {
     activePage: pagesList[0]
 });
 
+const uploadPageController = (req, res) => res.render(pagesList[1], {
+    pagesList,
+    activePage: pagesList[1]
+});
+
+const formPageController = (req, res) => res.render(pagesList[2], {
+    pagesList,
+    activePage: pagesList[2]
+});
+
+const findPageController = (req, res) => res.render(pagesList[3], {
+    pagesList,
+    activePage: pagesList[3]
+});
+
+const aboutUsPageController = (req, res) => res.render(pagesList[4], {
+    pagesList,
+    activePage: pagesList[4]
+});
 
 
 
-module.exports = {homePageController};
+
+module.exports = {homePageController, aboutUsPageController, uploadPageController, findPageController, formPageController};
--- router/views.js
+++ router/views.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const pagesList = require('../utils/pageList');
-const {homePageController} = require('../controller/views');
+const {homePageController, uploadPageController, formPageController, findPageController, aboutUsPageController} = require('../controller/views');
 
 const router = express.Router();
 
 router.get(`/${pagesList[0]}`, homePageController);
+router.get(`/${pagesList[1]}`, uploadPageController);
+router.get(`/${pagesList[2]}`, formPageController);
+router.get(`/${pagesList[3]}`, findPageController);
+router.get(`/${pagesList[4]}`, aboutUsPageController);
 
 module.exports = router;
